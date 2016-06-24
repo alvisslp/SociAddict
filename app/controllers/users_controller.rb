4b0b71af4e18331f0b82de775d7fed969a6144af
alvisslp <julian.colin@epita.fr>
2016-06-24 12:28:44 +0200

    bootstrap because beautiful, and corrected some bugs like diff between more_than and more_or_equal_to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -91,6 +91,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :referrer)
+      params.require(:user).permit(:name, :referrer, referrer_attributes: [:name])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,30 @@ class User < ActiveRecord::Base
   has_one :relationship, foreign_key: 'referral_id'
   has_one :referrer, :through => :relationship
   after_destroy :destroy_ref
+  accepts_nested_attributes_for :referrer
 
   def initialize(attributes = nil, options = {})
     unless attributes.nil?
-      # Change this line to allow 2 user with the same name
+      # Change this line to allow 2 user with the same name / depend on referrer attribution method / works with pseudonymes
       attributes['referrer'] = User.where(name: attributes['referrer']).first
     end
     super(attributes, options)
+  end
+
+  def update(attributes)
+    puts attributes.to_s
+    unless attributes['referrer_attributes'].nil?
+      # Change this lines to allow 2 user with the same name / depend on referrer attribution method / works with pseudonymes
+      self.referrer = nil
+      self.referrer = User.where(name: attributes['referrer_attributes']['name']).first unless attributes['referrer_attributes']['name'] == ''
+
+      attributes.delete('referrer_attributes')
+    end
+    unless attributes['referrer'].nil?
+      # Change this line to allow 2 user with the same name / depend on referrer attribution method
+      attributes['referrer'] = User.where(name: attributes['referrer']).first
+    end
+    super(attributes)
   end
 
   def original
@@ -29,6 +46,7 @@ class User < ActiveRecord::Base
   end
 
   def self.more_than(x)
+    return User.all if x.to_i < 0
     select('users.id, count(relationships.referrer_id) AS referrer_count')
         .joins(:relationships)
         .group('users.id')
@@ -38,7 +56,6 @@ class User < ActiveRecord::Base
   end
 
   def self.more_or_equal_to(x)
-    return User.all if x.to_i == 0
     more_than(x.to_i - 1)
   end
 
@@ -50,7 +67,6 @@ class User < ActiveRecord::Base
   private
 
   def destroy_ref
-    Relationship.where(referrer_id: id).destroy_all
-    Relationship.where(referral_id: id).destroy_all
+    Relationship.where('referrer_id = ? OR referral_id = ?', id, id).destroy_all
   end
 end
